a498042e4eab754254ff84a984e9a471eebe37e2
woberon <woberon@student.21-school.ru>
2021-12-03 17:16:56 +0300

    pre

--- get_next_line.h
+++ get_next_line.h
@@ -4,9 +4,12 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
-char	*get_next_line(int fd);
-int		ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char *s1, char *s2);
+char		*get_next_line(int fd);
+int			ft_strlen(const char *s);
+char		*ft_strjoin(char *s1, char *s2);
+int			ft_read(int fd, char *buff, int *i);
+int			check_next_line(char *line);
+static char	*check(const char *buff, int *i);
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -33,3 +33,53 @@ char	*ft_strjoin(char *s1, char *s2)
 	free(s2);
 	return (k);
 }
+int	check_next_line(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static char	*check(const char *buff, int *i)
+{
+	int		j;
+	char	*line2;
+
+	j = *i;
+	while (buff[j] != '\0' && buff[j] != '\n')
+		j++;
+	if (buff[j] == '\n')
+		j++;
+	line2 = (char *)malloc(j - *i + 1);
+	if (!line2)
+		return (NULL);
+	j = 0;
+	while (buff[*i] != '\0' && buff[*i] != '\n')
+	{
+		line2[j] = buff[*i];
+		j++;
+		(*i)++;
+	}
+	if (buff[*i] == '\n')
+		line2[j] = buff[(*i)++];
+	return (line2);
+}
+
+int	ft_read(int fd, char *buff, int *i)
+{
+	int	c;
+
+	c = read(fd, buff, BUFFER_SIZE);
+	if (c <= 0)
+		return (0);
+	*i = 0;
+	buff[c] = '\0';
+	return (c);
+}
