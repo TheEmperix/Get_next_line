cd14db8156bea59109e0fa2edf9a451cb186b5fa
woberon <woberon@student.21-school.ru>
2021-12-18 18:28:38 +0300

    finish

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woberon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/18 18:04:46 by woberon           #+#    #+#             */
+/*   Updated: 2021/12/18 18:25:31 by woberon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <stdio.h>
+
+char	ft_free(char *line1, char *line2)
+{
+	free(line1);
+	return (*line2);
+}
 
 char	*get_next_line(int fd)
 {
@@ -13,28 +30,17 @@ char	*get_next_line(int fd)
 		if (!ft_read(fd, buff, &i))
 			return (NULL);
 	}
-	line1 = check(buff, &i);
+	line1 = ft_check(buff, &i);
 	if (!line1)
 		return (NULL);
 	while (!check_next_line(line1))
 	{
 		if (!ft_read(fd, buff, &i))
 			return (line1);
-		line2 = check(buff, &i);
+		line2 = ft_check(buff, &i);
 		if (!line2)
-		{
-			free(line1);
-			return (line2);
-		}
+			ft_free(line1, line2);
 		line1 = ft_strjoin(line1, line2);
 	}
 	return (line1);
 }
-
-//int	main()
-//{
-//	int fd;
-//
-//	fd = open("text.txt", O_RDONLY);
-//	printf("%s", get_next_line(fd));
-//}
